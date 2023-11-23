6d8d2da29f0c1ab6a8fbffdab86494cfe7e183ef
iftekar2 <iftekar.hossain123@gmail.com>
2023-11-23 12:39:27 -0500

    Creating Pages and Components folders

--- dinapawsnew/src/App.js
+++ dinapawsnew/src/App.js
@@ -1,16 +1,10 @@
+import Home from "./Pages/Home";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello USA looool </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
       </header>
     </div>
   );
---
+++ dinapawsnew/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+import Navbar from "../Compoents/Navbar";
+
+function Home() {
+  return (
+    <div className="homePage">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
